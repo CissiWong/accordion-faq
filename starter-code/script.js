f51a9168fb7d973119f7cdeb6c23cd148dd0dffd
Hanna Pettersson <kontakta.hanna@gmail.com>
2017-10-26 17:20:32 +0200

    Adding starter code and design file

---
+++ starter-code/script.js
@@ -0,0 +1,7 @@
+// A function that adds and remove the class "active" on the section you click on.
+function toggle() {
+  this.classList.toggle("active")
+}
+
+//Selects and HTML element, and calls a function which will be executed when the element is clicked.
+document.getElementById("section1").onclick = toggle
